3e038e6b1a66e93ade0a3905157fca1774416477
myxcs <118356147+myxcs@users.noreply.github.com>
2024-01-16 18:16:20 +0700

    ui

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int cherries = 0;
+    private int apples = 0;
+    [SerializeField] private TextMeshPro cherriesText;
+    [SerializeField] private Text applesText;
+    //tạo trigger
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Cherry"))
+        {
+            cherries++;
+            //cherriesText.text = "Cherries: " + cherries;
+            Destroy(collision.gameObject);
+            Debug.Log(cherries);
+
+        }
+        if(collision.gameObject.CompareTag("Apple"))
+        {
+            
+            applesText.text = "Apples: " + apples;
+            apples++;
+            Destroy(collision.gameObject);
+        }
+    }
+}
