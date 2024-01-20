105614a080608fb2ce5799cc21d488e4bd2a461a
myxcs <118356147+myxcs@users.noreply.github.com>
2024-01-20 23:13:36 +0700

    death + traps

---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Trap")
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        anim.SetTrigger("death");
+        rb.bodyType = RigidbodyType2D.Static;
+    }
+
+    private void RestartLevel()
+    {
+           SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
